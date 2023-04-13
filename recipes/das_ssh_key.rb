c129910f78111c28b24b332fd9288d4470482806
Ermias <ermiasg@kth.se>
2023-04-13 18:00:21 +0200

    test default-config

--- recipes/das_ssh_key.rb
+++ recipes/das_ssh_key.rb
@@ -2,7 +2,7 @@ glassfish_user_home = conda_helpers.get_user_home(node['glassfish']['user'])
 
 nodedir=node['glassfish']['nodes_dir']
 
-# Add the master host's public key, so that it can start/stop the glassworks instance on this node using passwordless ssh.
+# Add the DAS node's public key, so that it can start/stop the work instances on this node using passwordless ssh.
 kagent_keys "#{glassfish_user_home}" do
   cb_user "#{node['glassfish']['user']}"
   cb_group "#{node['glassfish']['group']}"
